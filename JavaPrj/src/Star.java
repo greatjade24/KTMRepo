94639c1f612edf036e2b2439cea3dadb601ed426
DATA8320-19 <DATA8320-19@DESKTOP-TBPC9NM>
2020-05-08 11:27:16 +0900

    별찍기.

---
+++ JavaPrj/src/Star.java
@@ -0,0 +1,19 @@
+
+public class Star {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int j;
+		int k;
+		
+		for(i=0;i<6;i++) {
+			for(j=0;j<=i;j++) {
+				System.out.printf("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
