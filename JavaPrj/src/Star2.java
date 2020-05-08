285484ce6402c06d3a732082bcd7df266fa8b88b
DATA8320-19 <DATA8320-19@DESKTOP-TBPC9NM>
2020-05-08 11:44:52 +0900

    별찍기2

---
+++ JavaPrj/src/Star2.java
@@ -0,0 +1,28 @@
+
+public class Star2 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int j;
+		
+		
+		for(i=0;i<6;i++) {
+			for(j=0;j<=i;j++) {
+				System.out.printf("*");
+			}
+			
+			System.out.println();
+		}
+
+		for(i=0;i<6;i++) {
+			for(j=4;j>=i;j--) {
+				System.out.printf("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
