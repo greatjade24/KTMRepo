c20c911730efe0e9b9821c1c5406213c46df8515
DATA8320-19 <DATA8320-19@DESKTOP-TBPC9NM>
2020-05-08 11:35:07 +0900

    신규 프로젝트입니다.

--- JavaPrj/src/Star.java
+++ JavaPrj/src/Star.java
@@ -8,12 +8,26 @@ public class Star {
 		int k;
 		
 		for(i=0;i<6;i++) {
+			for(k=5;k>i;k--) {
+				System.out.printf(" ");
+			}
 			for(j=0;j<=i;j++) {
 				System.out.printf("*");
 			}
+			
 			System.out.println();
 		}
 
+		for(i=0;i<6;i++) {
+			for(k=0;k<=i;k++) {
+				System.out.printf(" ");
+			}
+			for(j=4;j>=i;j--) {
+				System.out.printf("*");
+			}
+			
+			System.out.println();
+		}
 	}
 
 }
