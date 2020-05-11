bf2e9a0560e875f7ba488671a27446b86d8df5e6
DATA8320-19 <DATA8320-19@DESKTOP-TBPC9NM>
2020-05-12 08:56:12 +0900

    계산기

--- JavaPrj/src/Ex02_02.java
+++ JavaPrj/src/Ex02_02.java
@@ -10,7 +10,9 @@ public class Ex02_02 {
 		
 		Scanner S = new Scanner(System.in);
 		
+		System.out.printf("첫번째 계산 할 값을 입력하세요 = ");
 		a = S.nextInt();
+		System.out.printf("두번째 계산 할 값을 입력하세요 = ");
 		b = S.nextInt();
 		
 		result = a + b;
