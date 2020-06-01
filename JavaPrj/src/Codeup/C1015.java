f15bb3ebd0b5dca31e5631f98c351e9a03b38257
greatjade24 <greatjade24@naver.com>
2020-06-02 08:08:01 +0900

    스캐너 사용법

---
+++ JavaPrj/src/Codeup/C1015.java
@@ -0,0 +1,18 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class C1015 {
+
+	public static void main(String[] args) {
+	
+		float a;
+		
+		Scanner s = new Scanner(System.in);
+		a = s.nextFloat();
+		
+		System.out.printf("%.2f", a);
+
+	}
+
+}
---
+++ JavaPrj/src/unicode.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class unicode{
+    public static void main(String[] args) {
+    	int a, b;
+    	
+    	Scanner s = new Scanner(System.in);
+    	a = s.nextInt();
+    	b = s.nextInt();
+    	
+    	System.out.printf("%d %d", a, b);
+    	
+    }
+}
