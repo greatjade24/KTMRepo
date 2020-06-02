60d02f6d46fb09819da2456850d55c7b7f258efe
greatjade24 <greatjade24@naver.com>
2020-06-02 10:21:55 +0900

    while문을 이용한 구구단

--- JavaPrj/src/while1.java
+++ JavaPrj/src/while1.java
@@ -3,28 +3,24 @@ public class while1 {
 
 	public static void main(String[] args) {
 		
-		int i;
-		int k;
+		int i, k;
 		
 		i = 3;
 		
 		while(i <= 8) {
-			if(i == 3)
-				System.out.println("<3´Ü>");
-			
-			else if(i == 5)
-				System.out.println("<5´Ü>");
+			if(i == 3 || i == 5)
+				System.out.println("<" + i + "´Ü>");
 			
 			k = 1;
 			
 			while(k <= 9) {
-				System.out.println(i + " * " + k + " = " + i * k);
+				System.out.println(i + " * " + k + " = " + i*k);
 				k++;
 			}
+			
 			System.out.println("");
+			
 			i++;
 		}
-
 	}
-
 }
