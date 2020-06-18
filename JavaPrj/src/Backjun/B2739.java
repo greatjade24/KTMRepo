39256e80540360aabe1e606a139780d5c380c8c7
greatjade24 <greatjade24@naver.com>
2020-06-19 08:48:23 +0900

    구구단 한단 출력

---
+++ JavaPrj/src/Backjun/B2739.java
@@ -0,0 +1,20 @@
+package Backjun;
+
+import java.util.Scanner;
+
+public class B2739 {
+
+	public static void main(String[] args) {
+		
+		int n, k;
+		
+		Scanner s = new Scanner(System.in);
+		n = s.nextInt();
+		
+		for(k = 1; k <= 9; k++)
+			System.out.println(n + " * " + k + " = " + n*k);
+		
+		
+	}
+
+}
