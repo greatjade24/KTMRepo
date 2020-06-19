7507207379cb23b90041394465b6faccdf0ff77e
greatjade24 <greatjade24@naver.com>
2020-06-20 03:14:51 +0900

    상속

---
+++ JavaPrj/src/AppleTree.java
@@ -0,0 +1,8 @@
+
+public class AppleTree extends Tree{
+		
+	/*@Override
+	public void treePrint() {
+		System.out.println("나는 자식입니다.");
+	}*/
+}
---
+++ JavaPrj/src/TreeTest.java
@@ -0,0 +1,13 @@
+
+public class TreeTest {
+
+	public static void main(String[] args) {
+		
+		AppleTree at = new AppleTree();
+	
+		at.treePrint();
+		
+		MyCreater mc = new MyCreater();
+	}
+
+}
