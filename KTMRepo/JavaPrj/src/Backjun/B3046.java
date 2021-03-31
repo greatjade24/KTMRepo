3763c95cf3a4f2534fcf0cbdb2acae6d29e9110f
greatjade24 <greatjade24@naver.com>
2021-03-31 10:01:25 +0900

    BackJoon dozen

---
+++ KTMRepo/JavaPrj/src/Backjun/B3046.java
@@ -0,0 +1,19 @@
+package Backjun;
+
+import java.util.Scanner;
+
+public class B3046 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int r1 = sc.nextInt();
+		int avg = sc.nextInt();
+		
+		int r2 = avg * 2 - r1;
+		
+		System.out.println(r2);
+	}
+
+}
---
+++ KTMRepo/JavaPrj/src/Backjun/B6749.java
@@ -0,0 +1,18 @@
+package Backjun;
+
+import java.util.Scanner;
+
+public class B6749 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int young = sc.nextInt();
+		int mid = sc.nextInt();
+		int dif = mid - young;
+		
+		System.out.println(mid + dif);
+	}
+
+}
