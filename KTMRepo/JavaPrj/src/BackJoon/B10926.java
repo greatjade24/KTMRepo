6ab0a1854ade0102e2f6f5e2b777190b34377ca2
greatjade24 <greatjade24@naver.com>
2021-04-06 08:42:36 +0900

    BackJoon No.10926

---
+++ KTMRepo/JavaPrj/src/BackJoon/B10926.java
@@ -0,0 +1,16 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class B10926 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.nextLine();
+		
+		System.out.println(id + "??!");
+	}
+
+}
