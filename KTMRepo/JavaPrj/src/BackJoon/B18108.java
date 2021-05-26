ea459a62359ef6e94b619dece76e3910bd9f700f
greatjade24 <greatjade24@naver.com>
2021-05-27 08:17:17 +0900

    BackJoon No.18108

---
+++ KTMRepo/JavaPrj/src/BackJoon/B18108.java
@@ -0,0 +1,16 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class B18108 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int bul = sc.nextInt();
+		
+		System.out.println(bul - 543);
+	}
+
+}
